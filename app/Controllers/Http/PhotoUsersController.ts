8fed121c340de0d0877a6e07357a045533913064
Luis Eduardo <luiseduess@gmail.com>
2022-12-14 21:34:00 -0300

    add uploads photos for s3 aws

--- app/Controllers/Http/PhotoUsersController.ts
+++ app/Controllers/Http/PhotoUsersController.ts
@@ -1,16 +1,32 @@
 import Drive from '@ioc:Adonis/Core/Drive'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import PhotoUser from 'App/Models/PhotoUser'
 
 export default class PhotoUsersController {
   public async store({ request, response, auth }: HttpContextContract) {
-    const photo = request.file('photo')
     const currentUser = auth.use('api').user
+    const photo = request.file('photo', {
+      size: '5mb',
+      extnames: ['png', 'jpg'],
+    })
 
-    if (photo) {
-      await Drive
-      return response.ok('Foto recebida.')
+    if (!photo) return response.status(400).json({ error: 'No file forwarded' })
+
+    if (!photo.isValid) {
+      return response.status(400).json({
+        error: `Your photo must have a maximum size of 5mb and be in .png or .jpg format`,
+      })
     }
 
-    return response.ok('Error')
+    await photo.moveToDisk('images', {}, 's3')
+
+    await PhotoUser.create({
+      url: photo.fileName,
+      userId: currentUser?.id,
+    })
+
+    return response.created()
   }
+
+  public async update() {}
 }
